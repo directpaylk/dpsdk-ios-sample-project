b6f4d2f6e4cb598a10c4ab6ec6e2ffbd2b9cd448
Deeptha Senanayake <chalithadeeptha@gmail.com>
2020-12-04 12:55:58 +0530

    sample project

---
+++ DPSDK.framework/Headers/DPSDK.h
@@ -0,0 +1,17 @@
+//
+//  DPSDK.h
+//  DPSDK
+//
+//  Created by Deeptha Senanayake on 3/27/19.
+//  Copyright © 2019 DirectPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for DPSDK.
+FOUNDATION_EXPORT double DPSDKVersionNumber;
+
+//! Project version string for DPSDK.
+FOUNDATION_EXPORT const unsigned char DPSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DPSDK/PublicHeader.h>
+
--- PaymentDemo/ViewController.swift
+++ PaymentDemo/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import DPSDK
 
 class ViewController: UIViewController {
-
+    var dpsdk:DPSDK!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dpsdk = DPSDK(apiKey: "API_KEY", dpMerchantId: "YOUR_MERCHANT_KEY", uniqueUserId: "USER_ID", userName: "USER_NAME", mobile: "+94731234567", email: "emailifavailable@user.com")
     }
 
-
+    @IBAction func addNewCard(_ sender: Any) {
+        dpsdk.addCard(self, success: { (c: Card) in
+            print("card added", c)
+        }) { (code: String, message: String) in
+            print("card adding failed", code, message)
+        }
+    }
 }
 
