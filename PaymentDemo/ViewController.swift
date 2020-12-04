c3024d064d82fdedb875074900e14536835d5a78
Deeptha Senanayake <chalithadeeptha@gmail.com>
2020-12-04 11:31:39 +0530

    Initial Commit

---
+++ PaymentDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentDemo
+//
+//  Created by Deeptha Senanayake on 12/4/20.
+//  Copyright © 2020 DirectPay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
